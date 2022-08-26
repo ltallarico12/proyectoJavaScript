2656b1fbf70caa762f561280fae57abf10b1a632
ltallarico12 <nanotallarico@gmail.com>
2022-08-26 19:51:37 -0300

    Terminado de primer entrega reformulada

--- index.js
+++ index.js
@@ -8,63 +8,30 @@ while (codigo != -1){
 
     //En caso de que el codigo del producto sea un número invalido(negativo), solicito ingresarlo nuevamente hasta que sea correcto.
     while (codigo <= 0){
-        let codigo = parseInt(prompt("Ingresar el codigo del producto correctamente"));
+        codigo = parseInt(prompt("Ingresar el codigo del producto correctamente1"));
     }
 
     //En caso de que el codigo del producto sea valido(mayor a 0).
     while (codigo > 0){
-        console.log();(`El codigo del producto ingresado es: ${codigo}`)
-        validar = prompt("validar con SI o NO en caso de que sea correcto el codigo ingressado").toUpperCase;
+        console.log(`El codigo del producto ingresado es: ${codigo}`);
+        validar = prompt("validar con SI o NO en caso de que sea correcto el codigo ingressado").toUpperCase();
 
         //Validación si el codigo del producto que quiero ingresar es correcto o no.
         if (validar === "SI"){
             precio = parseInt(prompt("Ingresar el precio del producto"));
-            console.log();(`El precio del pdoducto con codigo ${codigo} es: ${precio}`)
+            console.log(`El precio del pdoducto con codigo ${codigo} es: ${precio}`);
             monto = monto + precio
             codigo = parseInt(prompt("Ingresar el codigo de un nuevo producto"));
         }
         //En caso de no ser correcto el codigo del producto, se pide nuevamente.
         else{
-            codigo = parseInt(prompt("Ingresar el codigo del producto correctamente"));
+            codigo = parseInt(prompt("Ingresar el codigo del producto correctamente2"));
         }
     }
 }
 if(monto > 0){
     //Se muestra por pantalla el monto total que debe abonar el cliente.
-    console.log();(`El monto que el cliente debe abonar por su compra es de $${monto}`)
+    console.log(`El monto que el cliente debe abonar por su compra es de $${monto}`)
 }
 //Aviso de que ya se salió del programa.
-console.log();("Salimos del programa")
-
-
-
-
-/*
-let codigo = parseInt(prompt("Ingresar codigo del cliente \n Para finalizar ingresar -1"));
-let pago;
-
-while(codigo != -1){
-    while(codigo <= 0 ){
-        codigo = parseInt(prompt("Ingresar codigo del cliente valido"));
-    }
-    while (codigo > 0){
-        pago = prompt("Ingresar 'SI' o 'NO', según si pagó o no").toUpperCase();
-        if (pago === "SI"){
-            console.log(`El socio ${codigo} pagó`);
-        } 
-        else if(pago === "NO"){
-            console.log(`El socio ${codigo} no pagó`);
-        }
-        else{
-            while (pago != "SI" && pago != "NO"){
-                console.log("ERORR! \n Ingresar 'SI' en caso de que haya pagado o 'NO' en caso contrario");
-                pago = prompt("Ingresar 'SI' o 'NO'").toUpperCase();
-            }
-            
-        }
-        codigo = parseInt(prompt("Ingresar codigo del cliente"));
-    }
-}
-
-console.log("cierre");
-*/
+console.log("Salimos del programa");
