bcd79b54563da56faadca508a6f5086e561689ae
ltallarico12 <nanotallarico@gmail.com>
2022-09-27 13:11:47 -0300

    Modificado de agregado de carrito(sigue sin funcionar)

--- prueba.js
+++ prueba.js
@@ -17,11 +17,12 @@ function operaciones(op){
 //Creación de la clase producto con su constructor , para ir ingresando los productos al carrito.
 class Producto{
 
-    constructor(codigo, modelo, marca, tipo, precioUni, img){
-        this.codigo = codigo;
+    constructor(id, modelo, marca, tipo, cantidad, precioUni, img){
+        this.id = id;
         this.modelo = modelo;
         this.marca = marca;
         this.tipo = tipo;
+        this.cantidad = cantidad;
         this.precioUni = precioUni;
         this.img = img;
     }
@@ -29,69 +30,73 @@ class Producto{
 
 //creación del array.
 const productos = []
-const carrito = []
+let carrito = []
 let monto = 0;
 
 //Funcion para ir cargando los productos al array de los mismos.
 function cargarProductos(){
-    productos.push(new Producto(111, 'modelo1', 'bullpadel', 'paleta', 1000, 'https://res.cloudinary.com/dgvlsnajj/image/upload/v1664139230/paleta_dfmsly.webp'));
-    productos.push(new Producto(222, 'modelo2', 'adidas', 'paleta', 2000, 'https://res.cloudinary.com/dukzpfooc/image/upload/v1664142899/paleta_de_padel_nn2cxb.jpg'));
-    productos.push(new Producto(333, 'modelo3', 'bullpadel', 'paleta', 3000, 'https://res.cloudinary.com/dukzpfooc/image/upload/v1664143464/paleta_de_padel_jq02w4.webp'));
-    productos.push(new Producto(444, 'remeraA', 'vairo', 'ropa', 400, 'https://res.cloudinary.com/dukzpfooc/image/upload/v1664143565/remeraa_wvxo1l.jpg'));
-    productos.push(new Producto(555, 'remeraB', 'head', 'ropa', 500, 'https://res.cloudinary.com/dukzpfooc/image/upload/v1664143709/remerab_vdxzlk.webp'));
-    productos.push(new Producto(666, 'remeraC', 'x', 'ropa', 600, 'https://res.cloudinary.com/dgvlsnajj/image/upload/v1664139230/paleta_dfmsly.webp'));
+    productos.push(new Producto(111, 'modelo1', 'bullpadel', 'paleta', 1, 1000, 'https://res.cloudinary.com/dgvlsnajj/image/upload/v1664139230/paleta_dfmsly.webp'));
+    productos.push(new Producto(222, 'modelo2', 'adidas', 'paleta', 1, 2000, 'https://res.cloudinary.com/dukzpfooc/image/upload/v1664142899/paleta_de_padel_nn2cxb.jpg'));
+    productos.push(new Producto(333, 'modelo3', 'bullpadel', 'paleta', 1, 3000, 'https://res.cloudinary.com/dukzpfooc/image/upload/v1664143464/paleta_de_padel_jq02w4.webp'));
+    productos.push(new Producto(444, 'remeraA', 'vairo', 'ropa', 1, 400, 'https://res.cloudinary.com/dukzpfooc/image/upload/v1664143565/remeraa_wvxo1l.jpg'));
+    productos.push(new Producto(555, 'remeraB', 'head', 'ropa', 1, 500, 'https://res.cloudinary.com/dukzpfooc/image/upload/v1664143709/remerab_vdxzlk.webp'));
+    productos.push(new Producto(666, 'remeraC', 'x', 'ropa', 1, 600, 'https://res.cloudinary.com/dgvlsnajj/image/upload/v1664139230/paleta_dfmsly.webp'));
 }
 cargarProductos();
 console.log(productos);
 
 //Agrego los productos a la página.
-const itemsaPintar = document.querySelector("#carta-container");
-function agregarCards(){
+const itemsaPintar = document.getElementById('carta-container');
 
-    productos.forEach((producto) => {
-        let producHTML = `
-                    <div class="carta col-4" style="margin: 30px 0;">
-                        <h4>${producto.modelo}<h4>
-                        <div class="carta-img"><img src="${producto.img}" alt=""><div>
-                        <div class="carta-cuerpo">
-                            <strong>${producto.precioUni}</strong>
-                            <p class="text-muted py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                            <button id="comprar" class="btn btn-outline-dark">comprar</button>
-                        </div>
-                    <div>
-                    `;
-        itemsaPintar.innerHTML += producHTML;
-    });
-}
-agregarCards();
+productos.forEach((producto) => {
+    const productHTML = document.createElement('div')
+    productHTML.classList.add('carta')
+    productHTML.innerHTML = `
+                <h4>${producto.modelo}<h4>
+                <div class="carta-img"><img src="${producto.img}" alt=""></div>
+                <div class="carta-cuerpo">
+                    <strong>${producto.precioUni}</strong>
+                    <p class="text-muted py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <button id="btn${producto.id}" class="btn btn-outline-dark">comprar</button>
+                </div>
+                `;
+    itemsaPintar.appendChild(productHTML)
 
-
-//Muestro los productos del carrito.
-const carritoPintar = document.querySelector("#carrito-container");
-function mostrarProductosCarrito(){
-
-    carrito.forEach((producto) => {
-        let lineaCarritoHTML = `
-                            <div class="carrito-renglon">
-                                <div class="carrito-renglon-img"><img src="${producto.img}" alt=""><div>
-                                <strong>${producto.precioUni}<strong>
-                                <p>${producto.tipo}<p>
-                                <p>${producto.marca}<p>
-                            <div>
-                                `;
-        carritoPintar.innerHTML += lineaCarritoHTML;
-    });
-}
+    // //Tomo el id del boton para disparar el agregado del carrito.
+    // const boton = document.getElementById('btn${producto.id}');
+    // boton.addEventListener('click', () => {
+    //     agregarCarrito(producto.id)
+    // });
+});
+    
 
 
 
-const comprado = document.querySelector("#comprar");
-comprado.addEventListener("click", agregarCarrito);
 
-function agregarCarrito(id){
-    let produc = productos.find((produc) => produc.id === id);
+//Agregao al carrito
+function agregarCarrito(prodId){
+    const produc = productos.find((produc) => produc.id === prodId);
     carrito.push(produc);
+    console.log(carrito);
 }
-console.log(carrito);
+
+
+
+// //Muestro los productos del carrito.
+// const carritoPintar = document.querySelector("#carrito-container");
+// function mostrarProductosCarrito(){
+
+//     carrito.forEach((producto) => {
+//         let lineaCarritoHTML = `
+//                             <div class="carrito-renglon">
+//                                 <div class="carrito-renglon-img"><img src="${producto.img}" alt=""><div>
+//                                 <strong>${producto.precioUni}<strong>
+//                                 <p>${producto.tipo}<p>
+//                                 <p>${producto.marca}<p>
+//                             <div>
+//                                 `;
+//         carritoPintar.innerHTML += lineaCarritoHTML;
+//     });
+// }
 
 
